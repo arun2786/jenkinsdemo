61d704c141ce4cef11f2acfe2a9d108578ffeb88
Arun Kumar <java.kumar.arun@gmail.com>
2019-02-22 09:18:06 +0530

    first commit

---
+++ Table.java
@@ -0,0 +1,18 @@
+class Table {
+	public static void main(String... args){
+
+		int n = 5;
+
+		System.out.println("Number is : "+n);
+
+		for(int i=1;i<=10;i++){
+			System.out.print(n);
+			System.out.print("*");
+			System.out.print(i);
+			System.out.print("=");
+			System.out.print(n*i);
+			System.out.println();
+		}
+
+	}
+}
